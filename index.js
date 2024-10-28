74740c9a2afd03be82ab5a02dff690603a3cc42b
Tobias <tobias199@gmail.com>
2024-10-28 16:05:28 +0100

    first backend

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
+var express = require('express');
 require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+var app = express();
 const cors = require('cors');
-const config = require('./src/config/config');
-const router = require('./src/routes/router');
-
-const app = express();
-app.use(bodyParser.json());
 app.use(cors());
-app.use(router);
+app.use(express.json());
+const port = process.env.PORT
 
-const port = config.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: "florida57@ethereal.email",
+    pass: "2nYUP8CCPBthVrtw96",
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main(msg) {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+    to: "tobias199@gmail.com, tobias199@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: JSON.stringify(msg), // plain text body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+
+
+app.post("/submit",(request, response)=>{
+
+    console.log("body is:", request.body)
+
+    main(request.body).then(()=>{
+
+    });
+
+    console.log("HELLO")
+    response.status(200).json({ message: "Completed" }); // Use status and json directly
+
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
