bfef8ec52baa76df1bf9de0d3fcaaece0220158e
Tobias <tobias199@gmail.com>
2025-01-21 21:25:55 +0100

    gdpr att the bottom

--- src/componets/contactusv2.tsx
+++ src/componets/contactusv2.tsx
@@ -7,7 +7,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import Popupform from './popupform';
 import './contactusv2.css'
 import { LiveHelp } from '@mui/icons-material';
-
+import { Link } from 'react-router-dom';
 
 type Props = {
     setPopup: (s:string)=>void
@@ -53,7 +53,7 @@ function contactus({setPopup}:Props) {
         <iframe className="left" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17064.84017819241!2d12.086163091603263!3d57.68069408235191!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464ff116ba6d7f19%3A0x1b4425fcb78b0189!2sStarkare%20CrossFit!5e0!3m2!1ssv!2sse!4v1724234041052!5m2!1ssv!2sse"> </iframe> 
         </div>
 
-        <p className='copyright'>© Starkare Crossfit - All rights reserved | By Tobias Heidlund</p>
+        <p className='copyright'>© Starkare Crossfit - All rights reserved | By Tobias Heidlund</p> <Link to='/GDPR-POLICY'>GDPR policy</Link>
     </section>
 
 
