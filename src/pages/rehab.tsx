2607804d98455233cd3868fbdcb3387a8dd83793
Tobias <tobias199@gmail.com>
2025-03-02 13:27:59 +0100

    Update styling and content in card components and forms for improved layout and readability

--- src/pages/rehab.tsx
+++ src/pages/rehab.tsx
@@ -21,9 +21,9 @@ function rehab({setPopup}:Prop) {
     <h1>Är du i behov av manuell behandling?</h1>
     <h3>Om du har en långvarig smärtproblematik, eller om du har råkat ut för en akut skada kan vi på Starkare CrossFit hjälpa dig att bli av med dina smärtor i muskler och leder. Detta gör vi med hjälp av individanpassad rehab och manuell behandling.</h3>
     <section className='cardContaienr'>
-    <CardBox title={"Nybesök"} text={"Vid ett nybesök får du förklara din smärtproblematik för din terapeut. Efter konsultationen utförs ortopediska tester och en behandlingsplan tas fram."} boxname={"cardbox2"}/>
+    <CardBox title={"Nybesök"} text={"Vid ett nybesök får du förklara din smärtproblematik för din terapeut. Efter konsultationen utförs ortopediska tester och en behandlingsplan tas fram. \nPris:650kr"} boxname={"cardbox2"}/>
+    <CardBox title={"Återbesök"} text={"Under återbesöken får du ett individanpassat rehab-upplägg för att du ska kunna bli smärtfri. Övningarna gås igenom tillsammans med din terapeut i vårt gym. \nPris:500kr"}boxname={"cardbox2"}/>
     <CardBox title={"Rehab"} text={"Beroende på hur länge du upplevt din smärta behövs ett eller fler återbesök, detta sker i samråd med din terapeut."}boxname={"cardbox2"}/>
-    <CardBox title={"Återbesök"} text={"Under återbesöken får du ett individanpassat rehab-upplägg för att du ska kunna bli smärtfri. Övningarna gås igenom tillsammans med din terapeut i vårt gym."}boxname={"cardbox2"}/>
     <CardBox title={"Smärtfrihet"} text={"Målet med behandlingarna och rehab-träningen är att du på sikt ska bli smärtfri, kunna återgå till din vardag och att smärtproblematiken inte ska komma tillbaka."}boxname={"cardbox2"}/>
     </section>
     <button onClick={()=>{setPopup("Boka fysioterapeut")}}>Klicka här för att boka ett första besök hos vår fysioterapeut.</button>    
